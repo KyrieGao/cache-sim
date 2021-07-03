a82f04d44e687fc895fce39615b3c7c7a4dc205d
Shuai Gao <gshuai16@mail.ustc.edu.cn>
2021-07-03 19:16:36 +0800

    update code

--- test/lru_cache_tests.cpp
+++ test/lru_cache_tests.cpp
@@ -8,59 +8,63 @@ using lru_cache_t =
 
 TEST(CacheTest, SimplePut)
 {
-    lru_cache_t<std::string, int> cache(1);
+  lru_cache_t<std::string, int> cache(1);
 
-    cache.Put("test", 666);
+  cache.Put("test", 666);
 
-    EXPECT_EQ(cache.Get("test"), 666);
+  EXPECT_EQ(cache.Get("test"), 666);
 }
 
 TEST(CacheTest, MissingValue)
 {
-    lru_cache_t<std::string, int> cache(1);
+  lru_cache_t<std::string, int> cache(1);
 
-    EXPECT_THROW(cache.Get("test"), std::range_error);
+  EXPECT_THROW(cache.Get("test"), std::range_error);
 }
 
 TEST(CacheTest, KeepsAllValuesWithinCapacity)
 {
-    constexpr int CACHE_CAP = 50;
-    const int TEST_RECORDS = 100;
-    lru_cache_t<int, int> cache(CACHE_CAP);
+  constexpr int CACHE_CAP = 50;
+  const int TEST_RECORDS = 100;
+  lru_cache_t<int, int> cache(CACHE_CAP);
 
-    for (int i = 0; i < TEST_RECORDS; ++i)
-    {
-        cache.Put(i, i);
-    }
+  for (int i = 0; i < TEST_RECORDS; ++i)
+  {
+    cache.Put(i, i);
+  }
 
-    for (int i = 0; i < TEST_RECORDS - CACHE_CAP; ++i)
-    {
-        EXPECT_THROW(cache.Get(i), std::range_error);
-    }
+  for (int i = 0; i < TEST_RECORDS - CACHE_CAP; ++i)
+  {
+    EXPECT_THROW(cache.Get(i), std::range_error);
+  }
 
-    for (int i = TEST_RECORDS - CACHE_CAP; i < TEST_RECORDS; ++i)
-    {
-        EXPECT_EQ(i, cache.Get(i));
-    }
+  for (int i = TEST_RECORDS - CACHE_CAP; i < TEST_RECORDS; ++i)
+  {
+    EXPECT_EQ(i, cache.Get(i));
+  }
 }
 
-TEST(LRUCache, Remove_Test) {
+TEST(LRUCache, Remove_Test)
+{
   constexpr std::size_t TEST_SIZE = 10;
-  lru_cache_t <std::string, int> fc(TEST_SIZE);
+  lru_cache_t<std::string, int> fc(TEST_SIZE);
 
-  for (std::size_t i = 0; i < TEST_SIZE; ++i) {
+  for (std::size_t i = 0; i < TEST_SIZE; ++i)
+  {
     fc.Put(std::to_string(i), i);
   }
 
   EXPECT_EQ(fc.Size(), TEST_SIZE);
 
-  for (std::size_t i = 0; i < TEST_SIZE; ++i) {
+  for (std::size_t i = 0; i < TEST_SIZE; ++i)
+  {
     EXPECT_TRUE(fc.Remove(std::to_string(i)));
   }
 
   EXPECT_EQ(fc.Size(), 0);
 
-  for (std::size_t i = 0; i < TEST_SIZE; ++i) {
+  for (std::size_t i = 0; i < TEST_SIZE; ++i)
+  {
     EXPECT_FALSE(fc.Remove(std::to_string(i)));
   }
 }
